915acd4f38d66f4ee0c244b6b888e5f0d66efb81
MeisterYeti <ZordanZ@gmx.de>
2014-05-13 22:41:57 +0200

    bindings

--- Ext/ThesisSascha/E_Preprocessor.h
+++ Ext/ThesisSascha/E_Preprocessor.h
@@ -3,8 +3,11 @@
 #ifndef E_MINSG_THESISSASCHA_E_PREPROCESSOR_H_
 #define E_MINSG_THESISSASCHA_E_PREPROCESSOR_H_
 
-#include <EScript/Objects/ReferenceObject.h>
+#include <EScript/Objects/ExtReferenceObject.h>
 #include <MinSG/Ext/ThesisSascha/Preprocessor.h>
+#include <E_Util/E_ObjectFactory.h>
+#include <E_Util/E_Utils.h>
+#include <Util/Factory/WrapperFactory.h>
 
 
 namespace EScript {
@@ -20,14 +23,28 @@ namespace ThesisSascha{
 		E_Preprocessor ---|> EScript::ReferenceObject<Util::Reference<MinSG::ThesisSascha::Preprocessor> >
 			|
 			--------------> MinSG::ThesisSascha::SurfelManager		*/
-class E_Preprocessor : public EScript::ReferenceObject<Util::Reference<MinSG::ThesisSascha::Preprocessor> > {
+class E_Preprocessor : public EScript::ExtReferenceObject<Util::Reference<MinSG::ThesisSascha::Preprocessor>,EScript::Policies::EqualContent_ComparePolicy,E_Util::Policies::StoreAttrsInAttributeProvider> {
 		ES_PROVIDES_TYPE_NAME(Preprocessor)
+		static E_Util::E_ObjectFactory<MinSG::ThesisSascha::Preprocessor, E_Preprocessor> factorySystem;
+protected:
+	template<class Type, class E_Type>
+	static void addFactory() {
+		factorySystem.registerType(Type::getClassId(), Util::PolymorphicWrapperCreator<MinSG::ThesisSascha::Preprocessor, Type, E_Type>());
+	}
 	public:
+		static E_Preprocessor * create(MinSG::ThesisSascha::Preprocessor * preprocessor) {
+			if(preprocessor == nullptr) {
+				return nullptr;
+			}
+			return factorySystem.create(preprocessor->getTypeId(), preprocessor);
+		}
+		template<class, class, class> friend class Util::PolymorphicWrapperCreator;
+
 		static EScript::Type * getTypeObject();
 		static void init(EScript::Namespace & lib);
 
 		E_Preprocessor(Util::Reference<MinSG::ThesisSascha::Preprocessor> _obj) :
-				ReferenceObject_t(getTypeObject(), std::move(_obj)) {}
+				ExtReferenceObject_t(getTypeObject(), std::move(_obj)) {}
 		virtual ~E_Preprocessor() {}
 
 };
@@ -36,7 +53,7 @@ class E_Preprocessor : public EScript::ReferenceObject<Util::Reference<MinSG::Th
 
 ES_CONV_EOBJ_TO_OBJ(E_MinSG::ThesisSascha::E_Preprocessor, Util::Reference<MinSG::ThesisSascha::Preprocessor>, **eObj)
 ES_CONV_EOBJ_TO_OBJ(E_MinSG::ThesisSascha::E_Preprocessor, MinSG::ThesisSascha::Preprocessor *, (**eObj).get())
-ES_CONV_OBJ_TO_EOBJ(Util::Reference<MinSG::ThesisSascha::Preprocessor>, E_MinSG::ThesisSascha::E_Preprocessor, new E_MinSG::ThesisSascha::E_Preprocessor(obj))
+ES_CONV_OBJ_TO_EOBJ(Util::Reference<MinSG::ThesisSascha::Preprocessor>, E_MinSG::ThesisSascha::E_Preprocessor, obj.isNotNull() ? E_MinSG::ThesisSascha::E_Preprocessor::create(obj.get()) : nullptr)
 
 #endif // E_MINSG_THESISSASCHA_E_PREPROCESSOR_H_
 #endif // MINSG_EXT_THESISSASCHA
--- Ext/ThesisSascha/E_Renderer.cpp
+++ Ext/ThesisSascha/E_Renderer.cpp
@@ -9,8 +9,12 @@
 #include "E_Renderer.h"
 #include "E_SurfelManager.h"
 
+#include <E_MinSG/Core/Nodes/E_Node.h>
+#include <MinSG/Core/Nodes/Node.h>
+
 #include <EScript/Basics.h>
 #include <EScript/StdObjects.h>
+
 
 namespace E_MinSG {
 namespace ThesisSascha {
@@ -41,6 +45,81 @@ void E_Renderer::init(EScript::Namespace & lib) {
 		else
 			return EScript::create(new Renderer(parameter[0].to<SurfelManager*>(rt),parameter[1].to<std::string>(rt)));
 	})
+
+	{
+		struct ScriptedFunction{
+			EScript::Runtime & rt;
+			Renderer * owner;
+			ScriptedFunction(EScript::Runtime & _rt, Renderer * _owner) : rt(_rt), owner(_owner){}
+			float operator()(Node * node){
+				static const EScript::StringId handlerId("transitionStart");
+				EScript::ObjRef result = EScript::callMemberFunction(rt,EScript::create(owner),handlerId,
+															EScript::ParameterValues(EScript::create(node)));
+				return result.toFloat(200);
+			}
+		};
+
+		ES_MFUN(getTypeObject(),Renderer,"enableTransitionStartFn",0,0,
+					(thisObj->setTransitionStartFn(std::move(ScriptedFunction(rt,thisObj))),thisEObj))
+	}
+
+	{
+		struct ScriptedFunction{
+			EScript::Runtime & rt;
+			Renderer * owner;
+			ScriptedFunction(EScript::Runtime & _rt, Renderer * _owner) : rt(_rt), owner(_owner){}
+			float operator()(Node * node){
+				static const EScript::StringId handlerId("transitionEnd");
+				EScript::ObjRef result = EScript::callMemberFunction(rt,EScript::create(owner),handlerId,
+															EScript::ParameterValues(EScript::create(node)));
+				return result.toFloat(100);
+			}
+		};
+
+		ES_MFUN(getTypeObject(),Renderer,"enableTransitionEndFn",0,0,
+					(thisObj->setTransitionEndFn(std::move(ScriptedFunction(rt,thisObj))),thisEObj))
+	}
+
+	{
+		struct ScriptedFunction{
+			EScript::Runtime & rt;
+			Renderer * owner;
+			ScriptedFunction(EScript::Runtime & _rt, Renderer * _owner) : rt(_rt), owner(_owner){}
+			uint32_t operator()(Node * node, float size, uint32_t maxCount, float coverage){
+				static const EScript::StringId handlerId("countSurfels");
+				EScript::ObjRef result = EScript::callMemberFunction(rt,EScript::create(owner),handlerId,
+															EScript::ParameterValues(EScript::create(node),
+																	EScript::create(size),
+																	EScript::create(maxCount),
+																	EScript::create(coverage)));
+				return result.toUInt();
+			}
+		};
+
+		ES_MFUN(getTypeObject(),Renderer,"enableCountFn",0,0,
+					(thisObj->setCountFn(std::move(ScriptedFunction(rt,thisObj))),thisEObj))
+	}
+
+	{
+		struct ScriptedFunction{
+			EScript::Runtime & rt;
+			Renderer * owner;
+			ScriptedFunction(EScript::Runtime & _rt, Renderer * _owner) : rt(_rt), owner(_owner){}
+			float operator()(Node * node, float size, uint32_t maxCount, float coverage){
+				static const EScript::StringId handlerId("surfelSize");
+				EScript::ObjRef result = EScript::callMemberFunction(rt,EScript::create(owner),handlerId,
+															EScript::ParameterValues(EScript::create(node),
+																	EScript::create(size),
+																	EScript::create(maxCount),
+																	EScript::create(coverage)));
+				return result.toFloat(1);
+			}
+		};
+
+		ES_MFUN(getTypeObject(),Renderer,"enableSizeFn",0,0,
+					(thisObj->setSizeFn(std::move(ScriptedFunction(rt,thisObj))),thisEObj))
+	}
+
 }
 }
 
